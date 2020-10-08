8c41d78fa8f2c16d0d4105627e01ff2c33165fc9
Lotikadas <lotikadas.official@gmail.com>
2020-10-08 11:51:46 +0530

    This is my first program

---
+++ Python/HelloWorld.py
@@ -0,0 +1,28 @@
+
+print("Hello! I am Lotika 🤷‍♀️")
+
+x = 1
+y = 2
+
+print (x + y, "Hi There!" , sep=" ---- ", end= "\n")
+print("Hello, oi")
+
+
+
+
+
+
+
+# HOW TO WRITE COMMENTS
+
+'''
+1. Multi line comment
+Lorem
+Ipsum
+Doler
+Set amet
+solentius
+'''
+
+# 2. This is a single lne comment
+
